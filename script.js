bcf59526a7e745c002e2d6f3abd2eca5291a194c
jdelles <james.delles@gmail.com>
2021-06-25 23:35:29 -0400

    Calculator functioning!

--- script.js
+++ script.js
@@ -7,6 +7,18 @@ let display = document.querySelector(".display");
  * Select all the buttons for adding event listeners to each
  */ 
 let buttons = document.querySelectorAll("button"); 
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      keyPress(button.id); 
+    });
+  });
+
+/*
+ * Stores numbers and operations
+ */ 
+let firstNum = ''; 
+let secondNum = ''; 
+let operator = ''; 
 
 /*
  * Stores the most recent return value
@@ -61,3 +73,42 @@ function operation(num1, num2, operator) {
             break;
     }
 }
+
+function clear() {
+    display.textContent = ''; 
+    firstNum = ''; 
+    secondNum = ''; 
+    operator = ''; 
+}
+
+function backspace() {
+    display.textContent = display.textContent.toString().slice(0, -1); 
+}
+
+function storeNum() {
+    firstNum = Number.parseInt(display.textContent); 
+    display.textContent = ''; 
+}
+
+function keyPress(key) {
+    if (key === '=') {
+        secondNum = Number.parseInt(display.textContent); 
+        display.textContent = operation(firstNum, secondNum, operator); 
+    }
+    if (key === 'D') {
+        backspace(); 
+        return;  
+    }
+    if (key === 'C') {
+        clear(); 
+        return;  
+    }
+    if (key === '+' || key === '-' || key === '*' || key === '/') {
+        storeNum(); 
+        operator = key; 
+        return; 
+    }
+    if (key >= 0 && key < 9) {
+        display.textContent = display.textContent + key; 
+    }
+} 
